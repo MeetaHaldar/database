84f508e46e88278a779478f6ddf8ba6d1f4a3db7
meeta haldar <meetahaldar1001@gmail.com>
2021-11-19 14:15:12 +0530

    edit with online database

--- includes/config.php
+++ includes/config.php
@@ -1,17 +1,13 @@
-<?php 
+<?php
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','meeta');
-define('DB_PASS','1234');
-define('DB_NAME','database');
+define('DB_HOST', 'thorved.dns.army'); //thorved.dns.army:8081 phpmyadmin
+define('DB_USER', 'meeta');
+define('DB_PASS', 'meeta@2001');
+define('DB_NAME', 'meeta');
 // Establish database connection.
 $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+try {
+    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
 }
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
-}
-?>
